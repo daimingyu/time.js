08e9db2f668a639c7cbd2f14260dc5f5b4640655
daimingyu <2334491691@qq.com>
2018-11-17 17:50:24 +0800

    fix time object

--- time.js
+++ time.js
@@ -18,12 +18,6 @@
 
     //一些变量或常量
     let _date = new Date();
-    const PARTTEN_DATE = /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/;
-    const PARTTEN_DATE_HOUR = /^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$/;
-    const ERROR_PARTTEN = -1;
-    const ERROR_ILLEGAL = -2;
-    const FLAG_DATA = 1;
-    const FLAG_DATA_HOUR = 2;
     const UNIT_YEAR = 'year';
     const UNIT_MONTH = 'month';
     const UNIT_DAY = 'day';
@@ -46,162 +40,18 @@
         isLeapYear(year){
             return (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0);
         },
+        /**
+         * 判断用户输入的字串是否合法
+         * @param {用户输入的时间参数} timeStr 
+         */
+        validate(timeStr){
+            if(new Date(timeStr).toString() === 'Invalid Date'){
+                return false;
+            }
+            return true;
+        },
         cahngeToStandardTime(){
             //将不合法的时间转化成合法的时间
-        },
-        /**
-         * [校验用户输入的时间格式是否符合标准 注:只校验格式整不正确，而不校验是否合法]
-         * @param  {[string]} time [description]
-         * [-1] [不符合正则匹配]
-         * [2] [正确 并且格式为 YYYY-MM-DD HH:MM:SS]
-         * [1] [正确 并且格式为 YYYY-MM-D
-         */
-        validatePattern(time){
-            return  PARTTEN_DATE_HOUR.test(time) === true ? FLAG_DATA_HOUR :
-                    PARTTEN_DATE.test(time) === true ? FLAG_DATA :
-                    ERROR_PARTTEN;
-        },
-        /**
-         * [校验 年 是否合法，只要第一个字符不是0即是合法]
-         * @param  {[type]} year [description]
-         * @return {[type]}      [description]
-         */
-        validateYear(year){
-            year = typeof year === 'number' ? String(year) : year;
-            return year.charAt(0) === '0' ? false : true;
-        },
-        /**
-         * [校验 月 是否合法]
-         * @param  {[type]} month [description]
-         * @return {[type]}       [description]
-         */
-        validateMonth(month){
-            return parseInt(month) >= 1 && parseInt(month) <= 12 ? true : false;
-        },
-        /**
-         * [校验 日 是否合法]
-         * @param  {[type]} year  [description]
-         * @param  {[type]} month [description]
-         * @param  {[type]} day   [description]
-         * @return {[type]}       [description]
-         */
-        validateDay(year,month,day){
-            month = parseInt(month);
-            day = parseInt(day);
-            switch(month){
-                case 1:
-                    return day >= 1 && day <= 31 ? true : false;
-                case 2:
-                    return  this.isLeapYear(year)?
-                            day >= 1 && day <= 29 ? true : false:
-                            day >= 1 && day <= 28 ? true : false;
-                case 3:
-                    return day >= 1 && day <= 31 ? true : false;
-                case 4:
-                    return day >= 1 && day <= 30 ? true : false;
-                case 5:
-                    return day >= 1 && day <= 31 ? true : false;
-                case 6:
-                    return day >= 1 && day <= 30 ? true : false;
-                case 7:
-                    return day >= 1 && day <= 31 ? true : false;
-                case 8:
-                    return day >= 1 && day <= 31 ? true : false;
-                case 9:
-                    return day >= 1 && day <= 30 ? true : false;
-                case 10:
-                    return day >= 1 && day <= 31 ? true : false;
-                case 11:
-                    return day >= 1 && day <= 30 ? true : false;
-                case 12:
-                    return day >= 1 && day <= 31 ? true : false;
-            }
-        },
-        /**
-         * [校验 时 是否合法]
-         * @param  {[type]} hour [description]
-         * @return {[type]}      [description]
-         */
-        validateHour(hour){
-            return parseInt(hour) >= 1 && parseInt(hour) <= 24 ? true : false;
-        },
-        /**
-         * [校验 分 是否合法]
-         * @param  {[type]} minute [description]
-         * @return {[type]}        [description]
-         */
-        validateMinute(minute){
-            return parseInt(minute) >= 1 && parseInt(minute) <= 60 ? true : false;
-        },
-        /**
-         * [校验 秒 是否合法]
-         * @param  {[type]} second [description]
-         * @return {[type]}        [description]
-         */
-        validateSecond(second){
-            return parseInt(second) >= 1 && parseInt(second) <= 60 ? true : false;
-        },
-        /**
-         * [校验 YYYY-MM-DD 是否合法]
-         * @param  {[type]} time [description]
-         * @return {[type]}      [description]
-         */
-        validateIsDateLegal(date){
-            return this.validateYear(date.split('-')[0])?this.validateMonth(date.split('-')[1])?this.validateDay(date.split('-')[0],date.split('-')[1],date.split('-')[2])?true:false:false:false;
-        },
-        /**
-         * [校验 HH:MM:SS 是否合法]
-         * @param  {[type]} time [description]
-         * @return {[type]}      [description]
-         */
-        validateIsHourLegal(hour){
-            return this.validateHour(hour.split(':')[0])?this.validateMinute(hour.split(':')[1])?this.validateSecond(hour.split(':')[2])?true:false:false:false;
-        },
-        /**
-         * [校验 用户输入的日期 是否合法]
-         * @param  {[type]} time [description]
-         * @return {[type]}      [description]
-         */
-        validateLegal(time){
-            switch(this.validatePattern(time)){
-                case FLAG_DATA_HOUR:
-                    return this.validateIsDateLegal(time.split(' ')[0])?this.validateIsHourLegal(time.split(' ')[1])?true:false:false;
-                case FLAG_DATA:
-                    return this.validateIsDateLegal(time);
-                case ERROR_PARTTEN:
-                    return false;
-            }
-        },
-        /**
-         * [总体校验]
-         * @param  {[type]} time [description]
-         * @return 
-         * [-1] [不符合正则匹配 格式错误]
-         * [-2] [年 月 日 时 分 秒 不合法】
-         * [2] [正确 并且格式为 YYYY-MM-DD HH:MM:SS]
-         * [1] [正确 并且格式为 YYYY-MM-DD]
-         */
-        validate(time){
-            return  this.validatePattern(time) === ERROR_PARTTEN ? ERROR_PARTTEN :
-                    this.validateLegal(time) ? this.validatePattern(time) : 
-                    ERROR_ILLEGAL;
-        },
-        /**
-         * [校验用户输入的参数是否正确]
-         * @param  {[type]}  funcName  [函数名]
-         * @param  {[type]}  paramName [参数名]
-         * @param  {Boolean} isTime    [参数是否为tine字符串]
-         * @param  {[type]}  param     [参数]
-         * @param  {[type]}  type      [参数值类型]
-         * @return {[type]}            [description]
-         */
-        validateUserInput(funcName,paramName,isTime,param,type){
-            param === undefined ? this.throwError(`${funcName}(): ${paramName} 参数不能为空`) : null;
-            typeof param !== type ? _utils.throwError(`${funcName}(): ${paramName} 参数必须为 ${type}`) : null;
-            if(isTime){ 
-                this.validate(param) === ERROR_PARTTEN ? this.throwError(`${funcName}() ${paramName}: ${param} 参数格式错误`) : null;
-                this.validate(param) === ERROR_ILLEGAL ? this.throwError(`${funcName}() ${paramName}: ${param} 不合法`) : null;
-            }
         },
         /**
          * [报错函数]
@@ -212,111 +62,27 @@
             throw new Error(msg);
         },
         /**
-         * [将0-9的数字转化为字符串]
-         * @param  {[type]} num [description]
-         * @return {[type]}     [description]
+         * 判断是否是Time对象
+         * @param {*} obj 
          */
-        numStrAdd0(num){
-            return (parseInt(num) <= 9 && parseInt(num) >= 0) ? ('0' + num) : num;
-        },
-        /**
-         * 计算需要加多少月 addSomeDay函数 使用 参数必须是数字
-         */
-        addHowManyMonth(baseYear,baseMonth,baseDay,targetDay){
-            if(baseMonth == 1 || baseMonth == 3 || baseMonth == 5 || baseMonth == 7 || baseMonth == 8 || baseMonth == 10 || baseMonth == 12){
-                if((baseDay + targetDay) <= 31){
-                    return 0;
-                }else{
-                    return 1 + arguments.callee(baseYear,(baseMonth+1)%12 + 1,0,targetDay-baseDay);
-                }
-            }
-            if(baseMonth == 4 || baseMonth == 6 || baseMonth == 8 || baseMonth == 10 || baseMonth == 11){
-
-            }
-            if(this.isLeapYear(baseYear) && baseMonth == 2){
-
-            }
-            if(!this.isLeapYear(baseYear) && baseMonth == 2){
-
-            }
-        },
-        addSomeYear(obj,number){
-            let date = `${parseInt(obj.year) + number}-${obj.month}-${obj.day}`;
-            obj.time = obj.type === 1?date:`${date} ${obj.hour}:${obj.minute}:${obj.second}`;
-            this.updateTimeObj(obj);
-        },
-        addSomeMonth(obj,number){
-            let addYears = Math.floor((parseInt(number) + parseInt(obj.month))/13);
-            let newMonth = (parseInt(number) + parseInt(obj.month))%12;
-            newMonth = _utils.numStrAdd0(newMonth===0?12:newMonth);
-            let newYear = parseInt(obj.year) + addYears;
-            let date = `${newYear}-${newMonth}-${obj.day}`;
-            obj.time = obj.type === 1?date:`${date} ${obj.hour}:${obj.minute}:${obj.second}`;
-            this.updateTimeObj(obj);
-        },
-        addSomeDay(obj,number){
-
-        },
-        /**
-         * [判断某年 或者 某年某月有多少天]
-         * @param  {[type]} year  [description]
-         * @param  {[type]} month [description]
-         * @return {[type]}       [description]
-         */
-        howManyDays(year,month){
-            this.validateYear(year) === false ? this.throwError(`${year} 格式错误`) :null;
-            if(!month){
-                return this.isLeapYear(year) === true ? 366 : 365;
-            }else{
-                this.validateMonth(month) === false ? this.throwError(`${month} 格式错误`) :null;
-                let res = 31;
-                res = ((parseInt(month) === 4) || (parseInt(month) === 6) || (parseInt(month) === 8) || (parseInt(month) === 10))?30:31;
-                res = (parseInt(month) !== 2)? res : this.isLeapYear(year) === true ? 29 : 28 ;
-                return res;
-            }
-        },
-        /**
-         * [更新obj对象]
-         * @param  {[type]} obj [description]
-         * @return {[type]}     [description]
-         */
-        updateTimeObj(obj){
-            var timeStr = obj.time;
-            obj.year = timeStr.split(' ')[0].split('-')[0];
-            obj.month = timeStr.split(' ')[0].split('-')[1];
-            obj.day = timeStr.split(' ')[0].split('-')[2];
-            obj.hour = timeStr.split(' ')[1]?timeStr.split(' ')[1].split(':')[0]:undefined;
-            obj.minute = timeStr.split(' ')[1]?timeStr.split(' ')[1].split(':')[1]:undefined;
-            obj.second = timeStr.split(' ')[1]?timeStr.split(' ')[1].split(':')[2]:undefined;
+        isTimeObject(obj){
+            if(obj._date) return true;
+            return false;
         }
     };
 
     let Time = function(timeStr){
         //需要对用户输入的timeStr进行校验校验
-        _utils.validateUserInput('Time','timeStr',true,timeStr,'string')? _utils.throwError(`Time 参数格式错误`) : null;
-
+        if(!_utils.validate(timeStr)){
+            _utils.throwError('Invalid Date');
+        }
         //返回Time对象
         return new Time.prototype.init(timeStr);
     }
 
     //构造函数
     Time.prototype.init = function(timeStr){
-        this.time = timeStr;
-        this.year = timeStr.split(' ')[0].split('-')[0];
-        this.month = timeStr.split(' ')[0].split('-')[1];
-        this.day = timeStr.split(' ')[0].split('-')[2];
-
-        if(timeStr.split(' ')[1]){
-            this.hour = timeStr.split(' ')[1].split(':')[0];
-        }
-        if(timeStr.split(' ')[1]){
-            this.minute = timeStr.split(' ')[1].split(':')[1];
-        }
-        if(timeStr.split(' ')[1]){
-            this.second = timeStr.split(' ')[1].split(':')[2];
-        }
-
-        this.type = timeStr.split(' ')[1]?FLAG_DATA_HOUR:FLAG_DATA;
+        this._date = new Date(timeStr);
     } 
 
     //将Time对象的原型指向Time的原型，这样Time构造出的对象 就能使用Time原型上的方法了
@@ -418,7 +184,7 @@
             return result;
         },
         /**
-         * [判断是否是闰年]
+         * [判断某年是否是闰年]
          * @return {Boolean} [description]
          */
         isLeapYear(year){
@@ -431,61 +197,138 @@
          * @return {[type]}       [description]
          */
         howManyDays(year,month){
-            return _utils.howManyDays(year,month);
+            if(!month){
+                return this.isLeapYear(year) === true ? 366 : 365;
+            }else{
+                let res = 31;
+                res = ((parseInt(month) === 4) || (parseInt(month) === 6) || (parseInt(month) === 8) || (parseInt(month) === 10))?30:31;
+                res = (parseInt(month) !== 2)? res : this.isLeapYear(year) === true ? 29 : 28 ;
+                return res;
+            }
         },
-        countDown(){
-            //倒计时功能
-        },
+        /**
+         * 判断某年某月某日是星期几 
+         * @param {年} year 
+         * @param {月} month 
+         * @param {日} day 
+         * @returns 1,2,3,4,5,6,7
+         */
+        getWeek(year,month,day){
+            if(!_utils.validate(year + '-' + month + '-' +day)){
+                _utils.throwError('getWeek(): 参数错误');
+            }
+            let d = new Date(year + '-' + month + '-' + day);
+            if(d.getDay() === 0){
+                return 7;
+            }
+            return d.getDay();
+        }
     });
 
     //添加Time对象函数
     Time.prototype.extend({
         /**
+         * 获取格式化的时间 默认是YY-MM-DD hh:mm:ss
+         */
+        format(){
+            return this._date.getFullYear() + '-' + (this._date.getMonth() + 1) + '-' + this._date.getDate() + ' ' + this._date.getHours() + ':' + this._date.getMinutes() + ':' + this._date.getSeconds();
+        },
+        /**
          * [判断时间是否在time之前]
          * @param  {[type]}  time [description]
          * @return {Boolean}      [description]
          */
-        isBefore(time){
-            _utils.validateUserInput('isBefore','time',true,time,'string');
-            _utils.validate(this.time) !== _utils.validate(time) ? _utils.throwError(`${this.time} 和  ${time} 格式不一致,无法比较`) : null;
-            return this.time < time ? true : false;
+        isBefore(t){
+
+            if(!_utils.isTimeObject(t)){
+                if(typeof t === 'string'){
+                    t = Time(t);
+                }else{
+                    _utils.throwError('isBefore(): 参数有误');
+                }
+            }
+
+            if(this._date.getTime() < t._date.getTime()){
+                return true
+            }
+
+            return false;
         },
         /**
          * [判断时间是否在time之后]
-         * @param  {[type]}  time [description]
+         * @param  {[type]}  t [description]
          * @return {Boolean}      [description]
          */
-        isAfter(time){
-            _utils.validateUserInput('isAfter','time',true,time,'string');
-            _utils.validate(this.time) !== _utils.validate(time) ? _utils.throwError(`${this.time} 和  ${time} 格式不一致,无法比较`) : null;
-            return this.time > time ? true : false;
+        isAfter(t){
+            if(!_utils.isTimeObject(t)){
+                if(typeof t === 'string'){
+                    t = Time(t);
+                }else{
+                    _utils.throwError('isAfter(): 参数有误');
+                }
+            }
+
+            if(this._date.getTime() > t._date.getTime()){
+                return true;
+            }
+
+            return false;
         },
         /**
          * [判断time是否在time1和time2之间]
-         * @param  {[type]}  time1 [description]D
-         * @param  {[type]}  time2 [description]
+         * @param  {[type]}  t1 [description]
+         * @param  {[type]}  t2 [description]
          * @return {Boolean}       [description]
          */
-        isBetween(time1,time2){
-            _utils.validateUserInput('isBetween','time1',true,time1,'string');
-            _utils.validateUserInput('isBetween','time2',true,time2,'string');
-            ( _utils.validate(this.time) == _utils.validate(time1) && _utils.validate(this.time) == _utils.validate(time2) ) ? null : _utils.throwError(`${this.time}、${time1} 和 ${time2} 格式不一致,无法比较`) ;
-            return (this.time > time1 && this.time < time2) ? true : false;
+        isBetween(t1,t2){
+
+            if(!_utils.isTimeObject(t1)){
+                if(typeof t1 === 'string'){
+                    t1 = Time(t1);
+                }else{
+                    _utils.throwError('isBetween(): 参数有误');
+                }
+            }
+
+            if(!_utils.isTimeObject(t2)){
+                if(typeof t2 === 'string'){
+                    t2 = Time(t2);
+                }else{
+                    _utils.throwError('isBetween(): 参数有误');
+                }
+            }
+
+            if(this._date.getTime() > t1._date.getTime() && this._date.getTime() < t2._date.getTime()){
+                return true;
+            }
+
+            return false;
         },
+        /**
+         * 对日期进行加减操作 支持链式调用
+         * @param {要加的天数 整数为加n天 负数为减n天} number 
+         * @param {单位} unit 
+         */
         add(number,unit){
             switch(unit){
                 case UNIT_YEAR:
-                    _utils.addSomeYear(this,number);
+                    this._date.setYear(this._date.getFullYear() + parseInt(number));
                     return this;
                 case UNIT_MONTH:
-                    _utils.addSomeMonth(this,number);
+                    this._date.setMonth(this._date.getMonth() + parseInt(number));
                     return this;
                 case UNIT_DAY:
-                    _utils.addSomeDay(this,number);
+                    this._date.setDate(this._date.getDate() + parseInt(number));
                     return this;
                 case UNIT_HOUR:
+                    this._date.setHours(this._date.getHours() + parseInt(number));
+                    return this;
                 case UNIT_MINUTE:
+                    this._date.setMinutes(this._date.getMinutes() + parseInt(number));
+                    return this;
                 case UNIT_SECOND:
+                    this._date.setSeconds(this._date.getSeconds() + parseInt(number));
+                    return this;
                 default :
                     _utils.throwError(`单位 ${unit} 错误`);
             }
